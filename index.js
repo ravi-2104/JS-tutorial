de73025b607d23f7f2ff9ff544ce7dc71ac9712a
ravi2104 <rexxmandal@gmail.com>
2025-12-18 22:48:32 +0530

    add a string to check

---
+++ index.js
@@ -0,0 +1,82 @@
+//finding the second largest number form list of array;
+
+// let x = [4,20,10,5,7,1];
+
+// let large , secondLarge;
+// large = x[0];
+// for(let i=1; i<x.length; i++){
+
+//     if(large < x[i]){
+//         secondLarge = large;
+//         large = x[i];
+//     }
+//     else if(secondLarge < x[i]){
+//         secondLarge = x[i];
+//     }
+// }
+
+// console.log(`largest number is : ${large} and second largest number is : ${secondLarge}`);
+
+//---------------------------------------------
+
+// Average of an array;
+
+// let x = [4,20,10,5,7,1] , sum =0 , avg;
+
+// for(let i=0; i<x.length; i++){
+//     sum = sum + x[i];
+// }
+
+// avg = sum / (x.length);
+
+// console.log("sum is : "+sum);
+// console.log("your average : "+avg);
+
+//-------------------------------------------
+
+// let x = [4,20,10,5,7,1] , i;
+
+// let start = 1 , end = 3;
+
+// for(i = start; i<=end; i++){
+//     console.log("your value is : "+x[i]);
+// }
+
+//--------------------------------------------------
+
+// reverse the array
+
+// function reverseArray(input) {
+
+//   let str = [...input];
+//   for (let i = str.length-1; i >=0; i--) {
+//     console.log(str[i]);
+//   }
+
+// }
+// reverseArray("raj");
+
+//-------------------------------------------------
+
+
+// function reverseName(firstName, middleName, lastName) {
+//   let arr = [firstName, middleName, lastName];
+
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// reverseName("ravi","Raj","Sunny");
+
+
+// let arr =[{a:1},{a:2},{a:3},{a:4},{a:5}];
+
+// let arr =[1,2,3,4,5];
+// let arr1 = arr.includes(4);
+// let arr1 = arr.indexOf(4);
+// console.log(arr1);
+
+// console.log(arr.indexOf({a:3}));
+
+let x = "hello";
